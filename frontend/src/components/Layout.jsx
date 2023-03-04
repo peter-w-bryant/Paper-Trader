899f9254d6cdc23dee6bd61363db23e1ee3761cc
bryxli <bryanzero765@gmail.com>
2023-03-04 15:05:27 -0600

    login/register button

--- frontend/src/components/Layout.jsx
+++ frontend/src/components/Layout.jsx
@@ -1,9 +1,25 @@
-import { Outlet } from 'react-router-dom';
+import { useState } from 'react';
+import { Container, Nav, Navbar } from "react-bootstrap";
+import { Link, Outlet } from 'react-router-dom';
+
+import LoginContext from "../contexts/loginContext";
 
 function Layout() {
+    const [loggedIn, setLoggedIn] = useState(false);
+
     return (
         <>
-            <Outlet />
+            <LoginContext.Provider value={[loggedIn, setLoggedIn]}>
+                <Navbar bg='dark' variant='dark'>
+                    <Container>
+                        <Navbar.Brand as={Link} to='/'>Home</Navbar.Brand>
+                        <Nav className='ms-auto'>
+                            { loggedIn === false && <Nav.Link as={Link} to="login">Login / Register</Nav.Link> }
+                        </Nav>
+                    </Container>
+                </Navbar> 
+                <Outlet />
+            </LoginContext.Provider>
         </>
     );
 }
