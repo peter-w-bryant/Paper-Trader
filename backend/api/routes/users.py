800e84f63abb8039ce9002c0dfa2caada4563c2e
Forrest <126980017+f-dai@users.noreply.github.com>
2023-03-04 15:25:04 -0600

    Return 401 if user not found

--- backend/api/routes/users.py
+++ backend/api/routes/users.py
@@ -10,6 +10,8 @@ users = Blueprint('users', __name__)
 @users.route('/user-info/<user_id>', methods=['GET'])
 def get_user_info(user_id):
     user = User.query.filter_by(UID=user_id).first()
+    if not user:
+        return {401: 'Username not found!'}
     return jsonify(
         id=user.get_id(),
         username=user.get_username(),
