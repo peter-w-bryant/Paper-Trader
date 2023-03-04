e69a90e8669c808fd3e8a4872a82c0cc999ef666
Peter Bryant <72423203+peter-w-bryant@users.noreply.github.com>
2023-03-04 14:23:00 -0600

    /register implemented

--- backend/api/routes/auth.py
+++ backend/api/routes/auth.py
@@ -8,6 +8,8 @@ from flask import Blueprint, jsonify, current_app, render_template, redirect, ur
 from flask_login import UserMixin, login_user, LoginManager, login_required, logout_user, current_user # for handling user sessions
 from flask_bcrypt import Bcrypt # for hashing passwords
 from flask_sqlalchemy import SQLAlchemy
+# Import sqlalchemy.exc.IntegrityError
+from sqlalchemy.exc import IntegrityError
 
 auth = Blueprint('auth', __name__) # blueprint for auth routes
 
@@ -18,3 +20,35 @@ from models import User
 @auth.route('/')
 def home():
     return render_template('home.html')
+
+@auth.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegisterForm() # TODO remove
+    if request.method == 'POST':
+        try:
+            hashed_password = bcrypt.generate_password_hash(form.password.data)    # hash password
+            new_user = User(username=form.username.data, password=hashed_password) # create new user
+            db.session.add(new_user) # add new user to database
+            db.session.commit()      # commit changes to database
+            # users = User.query.all() # get all users
+            return {201: 'User created successfully!'} # return success message
+        except IntegrityError:
+            return {409: 'Username already exists!'}   # return error message
+
+    return render_template('register.html', form=form)
+
+class RegisterForm(FlaskForm):
+    username = StringField(validators=[
+                           InputRequired(), Length(min=4, max=20)], render_kw={"placeholder": "Username"})
+
+    password = PasswordField(validators=[
+                             InputRequired(), Length(min=8, max=20)], render_kw={"placeholder": "Password"})
+
+    submit = SubmitField('Register')
+
+    def validate_username(self, username):
+        existing_user_username = User.query.filter_by(
+            username=username.data).first()
+        if existing_user_username:
+            raise ValidationError(
+                'That username already exists. Please choose a different one.')
