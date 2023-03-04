6b6f17472e8424fb189c57b636fb2372016d527f
bryxli <bryanzero765@gmail.com>
2023-03-04 14:20:39 -0600

    layout home notfound

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,24 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
+import Layout from './components/Layout'
+import Home from './components/Home'
+import NotFound from './components/NotFound'
+
 import './App.css';
 
 function App() {
     return (
-        <h1>Paper Trader</h1>
+        <>
+            <h1>Paper Trader</h1>
+            <BrowserRouter>
+                <Routes>
+                    <Route path='/' element={<Layout />}>
+                        <Route index element={<Home />} />
+                        <Route path='*' element={<NotFound />} />
+                    </Route>
+                </Routes>
+            </BrowserRouter>
+        </>
     );
 }
 
