1db733a9f3f77ff745a999432b47ff098a87fbf0
fossifer <23732689+fossifer@users.noreply.github.com>
2023-03-04 15:04:07 -0600

    Basic frame for user-info

---
+++ backend/api/routes/users.py
@@ -0,0 +1,16 @@
+from flask import Blueprint, jsonify, current_app, request
+import json
+import os
+
+from db import db
+from models import User
+
+users = Blueprint('users', __name__)
+
+@users.route('/user-info', methods=['GET'])
+def get_user_info(user_id):
+    user = User.query.filter_by(UID=user_id).first()
+    return jsonify(
+        id=user.get_id(),
+        username=user.get_username(),
+    )
