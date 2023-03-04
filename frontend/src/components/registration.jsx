02842b94539f8bc29978054f847a34d7b79090c6
Shrey-Modi <shreymodi18@gmail.com>
2023-03-04 15:21:19 -0600

    switched to bootstrap

--- frontend/src/components/registration.jsx
+++ frontend/src/components/registration.jsx
@@ -1,19 +1,42 @@
-// create a registration component to register a new user
+import React from 'react';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+
 
 function registration() {
     return (
-        <div>
-            <h1>Registration</h1>
-            <form>
-                <label>Username</label>
-                <input type="text" name="username" />
-                <label>Password</label>
-                <input type="password" name="password" />
-                <label>Confirm Password</label>
-                <input type="password" name="confirm password" />
-            </form>
-        </div>
+            <Form>
+                <Form.Group controlId="formBasicEmail">
+                    <Form.Label>Username</Form.Label>
+                    <Form.Control placeholder="Enter Username" />
+                </Form.Group>
+                <Form.Group controlId="formBasicPassword">
+                    <Form.Label>New Password</Form.Label>
+                    <Form.Control type="password" placeholder="New Password" />
+                </Form.Group>
+                <Button variant="primary" type="submit" onClick={handleRegister}>
+                    Register
+                </Button>
+            </Form>
+                        
     );
+}
+
+function handleRegister() {
+    console.log("Registering");
+}
+
+function handleUsername() {
+    // check if username is > 4 characters
+    console.log("Username");
+}
+
+function handleConfirmPassword() {
+    console.log("Confirming Password");
+}
+
+function handleNewPassword() {
+    console.log("New Password");
 }
 
 export default registration;
