bc0b88454b964079f1a8791c4c97cd193723cb7a
Peter Bryant <72423203+peter-w-bryant@users.noreply.github.com>
2023-03-04 17:34:25 -0600

    updates

--- backend/api/routes/stocks.py
+++ backend/api/routes/stocks.py
@@ -3,8 +3,14 @@ import json
 import os
 from datetime import datetime
 from yahoo_fin.stock_info import get_data, get_live_price
+from flask_login import UserMixin, login_user, LoginManager, login_required, logout_user, current_user # for handling user sessions
+
 
 stocks = Blueprint('stocks', __name__)
+
+from app import bcrypt, login_manager # import bcrypt and login_manager from app.py
+from db import db
+from models import User, Orders
 
 # import from resources/all_tickers.json
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -40,4 +46,45 @@ def get_current_stock_price(ticker):
             'current_price': stock_info
         }
         return stock_info
+    
+@stocks.route('/execute_order', methods=['GET', 'POST'])
+@login_required
+def execute_order():
+    """Executes new order"""
 
+    # ticker = request.args.get('ticker') # TODO
+    ticker = 'AAPL' # remove TODO
+
+    # investment = request.args.get('investment') # TODO
+    investment = 100 # remove TODO
+
+    UID = current_user.UID # get current user's UID
+    balance = current_user.balance # get current user's balance
+
+    if investment > balance:
+        return {400: 'Insufficient funds'} # return error message
+    
+    
+    ticker_current_price = get_live_price(ticker) # get current price of ticker
+    share_quantity = investment / ticker_current_price # Calculate number of shares
+
+    # Create new order
+    new_order = Orders(
+        UID=UID,
+        ticker=ticker,
+        quantity= share_quantity,
+        is_sold=False,
+        date=datetime.now()
+    )
+
+    # Add new order to db
+    db.session.add(new_order)
+
+    # Update user's balance
+    user = User.query.filter_by(UID=UID).first()
+    user.balance = balance - investment
+
+    # Commit changes to db
+    db.session.commit()
+
+    return {200: 'Order executed successfully!', 'balance': user.balance} # return success message
