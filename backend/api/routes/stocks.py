044edbf83e7f526011cb9e3047c67f40bbe7eff4
Peter Bryant <72423203+peter-w-bryant@users.noreply.github.com>
2023-03-04 22:37:46 -0600

    fixed historical-stock-info

--- backend/api/routes/stocks.py
+++ backend/api/routes/stocks.py
@@ -29,8 +29,7 @@ def get_historical_stock_info(ticker):
     current_date = datetime.now().strftime('%m/%d/%Y') # today's date
     
     if request.method == 'GET':
-        data = request.get_json()
-        start_date = data['start_date'] # get start_date from query string
+        start_date = request.args['start_date'] # get start_date from query string
         stock_info = get_data(ticker, start_date=f'{start_date}', end_date=f'{current_date}') # get stock info
         stock_info = stock_info.to_json(orient='index') # convert to json
         stock_info = json.loads(stock_info) # convert to dict
