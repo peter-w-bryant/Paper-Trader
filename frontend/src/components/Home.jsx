41b02c685909fc194d85e38ed03b7749892be2c0
Shrey-Modi <shreymodi18@gmail.com>
2023-03-04 20:27:52 -0600

    tested react high charts

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,10 +1,12 @@
 import All from './All'
+import Graph from './Graph';
 
 function Home() {
     return (
         <>
             <h1>Paper Trade Home Page</h1>
             <All />
+            <   Graph />
         </>
     );
 }
