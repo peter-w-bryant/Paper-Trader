80edd0fefa61a3f5f61e2cbcf27786036a242f0b
Forrest <126980017+f-dai@users.noreply.github.com>
2023-03-04 21:51:23 -0600

    Add a sample search box

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,9 +1,11 @@
 import All from './All'
+import Autosuggest from './Autosuggest';
 
 function Home() {
     return (
         <>
             <h1>Paper Trade Home Page</h1>
+            <Autosuggest/>
             <All />
         </>
     );
