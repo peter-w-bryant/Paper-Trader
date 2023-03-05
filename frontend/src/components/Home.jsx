6e2617ef4dc6a010163d141c7dc5ab7c2ebab9c1
Shrey-Modi <shreymodi18@gmail.com>
2023-03-04 20:58:05 -0600

    testing donuts

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,12 +1,13 @@
 import All from './All'
 import Graph from './Graph';
+import Donut from './donut';
 
 function Home() {
     return (
         <>
             <h1>Paper Trade Home Page</h1>
             <All />
-            <   Graph />
+            <   Donut />
         </>
     );
 }
